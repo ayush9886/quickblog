2a4fedac39ce5a07323e99e0ab33c2891cf6b314
Ayush Raj <ayushraj9886@gmail.com>
2025-08-28 02:14:23 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+import BlogList from '../components/BlogList.jsx'
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <BlogList/>
+    </>
+  )
+}
+
+export default Home
